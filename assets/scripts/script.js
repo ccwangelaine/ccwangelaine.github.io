4a6b57b0f958c1da52be8f40a2c429dee65b16c5
Elaine Wang <wangelaine@berkeley.edu>
2020-05-01 15:46:47 -0700

    for deploy

---
+++ assets/scripts/script.js
@@ -0,0 +1,40 @@
+
+let about1 = document.getElementById("nav-text-about");
+about1.onclick = function () {
+  document.getElementById('second-view').scrollIntoView({behavior: "smooth"});
+};
+
+let experience1 = document.getElementById("nav-text-experience");
+experience1.onclick = function () {
+  document.getElementById('third-view').scrollIntoView({behavior: "smooth"});
+};
+
+let main2 = document.getElementById("main2");
+main2.onclick = function () {
+  document.getElementById('first-view').scrollIntoView({behavior: "smooth"});
+};
+
+let experience2 = document.getElementById("experience2");
+experience2.onclick = function () {
+  document.getElementById('third-view').scrollIntoView({behavior: "smooth"});
+};
+
+let main3 = document.getElementById("main3");
+main3.onclick = function () {
+  document.getElementById('first-view').scrollIntoView({behavior: "smooth"});
+};
+
+let about3 = document.getElementById("about3");
+about3.onclick = function () {
+  document.getElementById('second-view').scrollIntoView({behavior: "smooth"});
+};
+
+let mail = document.getElementById("mail");
+mail.onclick = function () {
+  document.getElementById('second-view').scrollIntoView({behavior: "smooth"});
+};
+
+let phone = document.getElementById("phone");
+phone.onclick = function () {
+  document.getElementById('second-view').scrollIntoView({behavior: "smooth"});
+};
